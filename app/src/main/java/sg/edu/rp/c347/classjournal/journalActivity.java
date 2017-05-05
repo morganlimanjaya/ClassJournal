c89913d3d9c6caf9418d28e6fd83cfc9ffde430d
bryanlmk <mrpanda100@gmail.com>
2017-05-05 15:24:07 +0800

    a

--- app/src/main/java/sg/edu/rp/c347/classjournal/journalActivity.java
+++ app/src/main/java/sg/edu/rp/c347/classjournal/journalActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c347.classjournal;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class journalActivity extends AppCompatActivity{
     ListView lvWeek;
     ArrayAdapter aa;
     ArrayList<journal> a1;
+    Button btnEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class journalActivity extends AppCompatActivity{
         setContentView(R.layout.class_journal_info);
 
         lvWeek = (ListView)findViewById(R.id.listView1);
+        btnEmail = (Button)findViewById(R.id.buttonEmail);
 
         a1 = new ArrayList<>();
         a1.add(new journal("week 1", 'B'));
