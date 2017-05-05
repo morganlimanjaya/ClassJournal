df4ed13f57cc65cc62dfbe68157d1d2b45f9edc6
bryanlmk <mrpanda100@gmail.com>
2017-05-05 15:49:27 +0800

    aa

--- app/src/main/java/sg/edu/rp/c347/classjournal/journalActivity.java
+++ app/src/main/java/sg/edu/rp/c347/classjournal/journalActivity.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 
 public class journalActivity extends AppCompatActivity{
     Button btnInfo;
+    Button btnEmail;
     ListView lvWeek;
     ArrayAdapter aa;
     ArrayList<journal> a1;
@@ -28,6 +29,7 @@ public class journalActivity extends AppCompatActivity{
 
         lvWeek = (ListView)findViewById(R.id.listView1);
         btnInfo = (Button)findViewById(R.id.buttonInfo);
+        btnEmail = (Button)findViewById(R.id.buttonEmail);
         btnInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
@@ -42,6 +44,25 @@ public class journalActivity extends AppCompatActivity{
         a1.add(new journal("week 1", 'B'));
         a1.add(new journal("week 2", 'C'));
         a1.add(new journal("week 3", 'A'));
+        btnEmail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // The action you want this intent to do;
+                // ACTION_SEND is used to indicate sending text
+                Intent email = new Intent(Intent.ACTION_SEND);
+                // Put essentials like email address, subject & body text
+                email.putExtra(Intent.EXTRA_EMAIL,
+                        new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_TEXT,"Hi Faci,\n I am meow \n Please see my remarks so far, thank you! \n ");
+                // This MIME type indicates email
+                email.setType("message/rfc822");
+                // createChooser shows user a list of app that can handle
+                // this MIME type, which is, email
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client :"));
+
+            }});
+
 
     }
 }
