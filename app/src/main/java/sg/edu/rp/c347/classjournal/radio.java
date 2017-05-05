4e84302fb2db1ba8020f669b68a07e0285c0f186
bryanlmk <mrpanda100@gmail.com>
2017-05-05 15:02:37 +0800

    haha

--- app/src/main/java/sg/edu/rp/c347/classjournal/radio.java
+++ app/src/main/java/sg/edu/rp/c347/classjournal/radio.java
@@ -10,27 +10,9 @@ public class radio extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.radio);
 
-        btnEmail.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View arg0) {
-                // The action you want this intent to do;
-                // ACTION_SEND is used to indicate sending text
-                Intent email = new Intent(Intent.ACTION_SEND);
-                // Put essentials like email address, subject & body text
-                email.putExtra(Intent.EXTRA_EMAIL,
-                        new String[]{"jason_lim@rp.edu.sg"});
-                email.putExtra(Intent.EXTRA_TEXT,"Hi Faci,\n I am meow \n Please see my remarks so far, thank you! \n ");
-                // This MIME type indicates email
-                email.setType("message/rfc822");
-                // createChooser shows user a list of app that can handle
-                // this MIME type, which is, email
-                startActivity(Intent.createChooser(email,
-                        "Choose an Email client :"));
 
-            }});
-    }
 
 }
 }
